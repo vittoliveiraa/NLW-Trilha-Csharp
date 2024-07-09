d2342592f467eefdc86dac26e79cd22aff56479a
vitto <vitto@ITO>
2024-07-09 10:54:03 -0300

    Adicionar arquivos de projeto.

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,44 @@
+﻿using Journey.Application.UseCases.Trips.GetAll;
+using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Journey.Exception;
+using Journey.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterTripJson request)
+        {
+            try
+            {
+                var useCase = new RegisterTripUseCase();
+
+               var response =  useCase.Execute(request);
+
+                return Created(string.Empty, response);
+            }
+            catch (JourneyException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ResourceErrorMessages.INTERNAL_SERVER_ERROR);
+            }
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var useCase = new GetAllTripsUseCase();
+            var result = useCase.Execute();
+
+            return Ok(result);
+        }
+    }
+}
